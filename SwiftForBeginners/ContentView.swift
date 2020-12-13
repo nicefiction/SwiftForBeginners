52b6ebdb8efa9122cd367d9e45895e29564a6c35
Olivier Van hamme <nicefiction@gmail.com>
2020-12-13 10:28:12 +0100

    Initial Commit

---
+++ SwiftForBeginners/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftForBeginners
+//
+//  Created by Olivier Van hamme on 13/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftForBeginners/SwiftForBeginnersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftForBeginnersApp.swift
+//  SwiftForBeginners
+//
+//  Created by Olivier Van hamme on 13/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftForBeginnersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
