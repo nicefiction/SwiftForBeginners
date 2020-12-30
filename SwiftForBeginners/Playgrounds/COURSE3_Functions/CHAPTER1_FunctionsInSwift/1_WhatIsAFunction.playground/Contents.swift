349f7bedb0e4b61b5be90356e276f51f9f97797d
Olivier Van hamme <nicefiction@gmail.com>
2020-12-30 17:32:45 +0100

    Completes What is a Function , Course 3 Chapter 1 .

---
+++ SwiftForBeginners/Playgrounds/COURSE3_Functions/CHAPTER1_FunctionsInSwift/1_WhatIsAFunction.playground/Contents.swift
@@ -0,0 +1,78 @@
+import Foundation
+
+
+/**
+ `COURSE 3`
+ `Functions in Swift`
+ INTRO —A function is
+ a fundamental building block of a computer program
+ and allows us
+ to write chunks of code
+ that we can reuse
+ as often as we need .
+ In Swift , functions are quite powerful
+ and in this course,
+ we'll start by taking a look at the syntax
+ and how functions work .
+ Once we have the fundamentals underway ,
+ we'll look at how we can customize our parameter names ,
+ add default values
+ and return multiple values from a function call .
+ 
+ What you'll learn :
+ • Functions
+ • Parameters
+ • Named Parameters
+ • Default Values
+ • Multiple Return Values
+ */
+
+
+
+/**
+ `CHAPTER 1`
+ `Functions in Swift`
+ INTRO CHAPTER 1 —A function is a fundamental building block of a computer program
+ and allows us to write chunks of code that we can reuse as often as we need .
+ In this set of videos ,
+ we start by understanding what a function is
+ and why we need them .
+ Then we take a look at the syntax
+ and various attributes of a function .
+ */
+/**
+ `1 What is a Function ?`
+ INTRO —In this video , we take a conceptual look at
+ what a function is
+ and what we can do with them .
+ */
+/**
+ Until now ,
+ all the code we have written in our previous courses was executed line by line ,
+ from top to bottom in our playgrounds . What if we don't want to do that ?
+ Sometimes we may want a certain block of code to be executed only when we need it .
+ Now you might be thinking this is what control flow statements are for , right ?
+ Well , control flow statements are good for executing code based on logical conditions ,
+ but what we are looking for , is ,
+ a self-contained block of code that performs a very specific task .
+ This is exactly what a function is ,
+ a sequence of instructions that perform a very specific task ,
+ packaged up as a unit .
+ You can think of the coffee maker as a function .
+ It performs a very specific set of tasks in order to make coffee .
+ 
+ `WIKIPEDIA` : In computer programming ,
+ a `subroutine` is a sequence of program instructions
+ that performs a specific task ,
+ packaged as a unit .
+ This unit can then be used in programs wherever that particular task should be performed .
+ Subprograms may be defined within programs ,
+ or separately in libraries that can be used by many programs .
+ In different programming languages , a subroutine may be called
+ a procedure ,
+ a function ,
+ a routine ,
+ a method ,
+ or a subprogram .
+ The generic term callable unit is sometimes used .
+ */
