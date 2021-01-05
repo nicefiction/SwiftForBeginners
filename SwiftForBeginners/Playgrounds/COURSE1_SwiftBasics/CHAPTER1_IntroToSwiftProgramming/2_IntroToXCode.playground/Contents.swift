4e34cce13171cacb64cc03089fc955eb98c6b7c7
Olivier Van hamme <nicefiction@gmail.com>
2021-01-05 22:57:27 +0100

    Completes an Introduction to Xcode , Course 1 Chapter 1 .

---
+++ SwiftForBeginners/Playgrounds/COURSE1_SwiftBasics/CHAPTER1_IntroToSwiftProgramming/2_IntroToXCode.playground/Contents.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+
+/**
+ `2 An introduction to Xcode`
+ INTRO —Before we jump into writing code and learning about programming ,
+ let's spend a quick second talking about what Swift is .
+ There are many programming languages out there
+ and it's worth understanding
+ why we are learning Swift
+ and what it is used for .
+ */
+/**
+ Xcode , or IDE —Integrated Development Environment
+ provides everything we need to build an app ,
+ all in one place :
+ a Code Editor ,
+ an Interface builder ,
+ and a Console .
+ 
+ `2.1 Comments`
+ • Documenting functionality
+ • Instructions for coworkers
+ • Instructions for future developers on the project
+ 
+ `2.2 Customising the code editor`
+ In the meny bar :
+ XCode > Preferences
+ */
