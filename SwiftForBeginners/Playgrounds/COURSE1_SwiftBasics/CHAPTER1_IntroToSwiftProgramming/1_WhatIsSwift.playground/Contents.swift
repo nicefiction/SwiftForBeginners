a6236898d8d33a3dd384144b620302cbb117d151
Olivier Van hamme <nicefiction@gmail.com>
2021-01-05 22:53:28 +0100

    Completes What is Swift , Course 1 Chapter 1 .

---
+++ SwiftForBeginners/Playgrounds/COURSE1_SwiftBasics/CHAPTER1_IntroToSwiftProgramming/1_WhatIsSwift.playground/Contents.swift
@@ -0,0 +1,64 @@
+import Foundation
+
+
+/**
+ `COURSE 1`
+ `Swift Basics`
+ INTRO —Welcome to the world of Swift development !
+ In this course ,
+ we are going to learn how to program from absolute scratch
+ using Swift
+ and learn many fundamental concepts
+ that will get you started writing code immediately .
+ No prior programming experience is needed
+ and we are going to have a lot of fun !
+ */
+
+
+/**
+ `CHAPTER 1`
+ `An Introduction to Swift Programming .`
+ INTRO CHAPTER 1 —We'll begin our foray into programming by understanding
+ a very simple concept
+ - how to store and retrieve information ( also called data )
+ using constructs
+ called variables and constants .
+ Before we can do that however ,
+ we need to review our tool set ,
+ and in this set of videos,
+ we'll take a look at what Xcode is,
+ and the environment we're going to learn in - Playgrounds .
+ After that , we’ll dive right in , and look at
+ how to create and use variables and constants ,
+ how to properly name them
+ and what data means in programming .
+ */
+/**
+ `1 What is Swift ?`
+ INTRO —Before we jump into writing code and learning about programming ,
+ let's spend a quick second talking about what Swift is .
+ There are many programming languages out there
+ and it's worth understanding
+ why we're learning Swift and what it is used for .
+ */
+/**
+ `1.1 Types`
+ In a spoken language,
+ the building blocks are letters and resulting vocabulary .
+ In a Programming language ,
+ the building blocks are called `Types` .
+ Types are a construct to represent different kinds of data ,
+ like letters or numbers .
+ 
+ `1.2 Syntax`
+ In a spoken language ,
+ the rules ,
+ from which we construct words and sentences ,
+ are called grammar .
+ Similarly , in a programming language ,
+ we also have a set of rules , and we call it `Syntax` .
+ Syntax are commands ,
+ special words ,
+ and punctuation
+ you use to put together a program .
+ */
