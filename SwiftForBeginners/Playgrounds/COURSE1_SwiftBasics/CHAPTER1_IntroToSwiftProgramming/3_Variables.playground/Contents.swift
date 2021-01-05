9c2104098f45d05b1d8b97ce3866dd42f6bd2331
Olivier Van hamme <nicefiction@gmail.com>
2021-01-05 23:04:20 +0100

    Completes Variables , Course 1 Chapter 1 .

---
+++ SwiftForBeginners/Playgrounds/COURSE1_SwiftBasics/CHAPTER1_IntroToSwiftProgramming/3_Variables.playground/Contents.swift
@@ -0,0 +1,49 @@
+import Foundation
+
+
+/**
+ `3 Variables`
+ INTRO —Programming has a lot to do with information .
+ You store information ,
+ retrieve information ,
+ and then you manipulate it .
+ Learn how to store and retrieve information using variables .
+ */
+/**
+ A variable is a construct to hold on to data .
+ When we use data in a computer ,
+ when we assign something to a variable ,
+ it is actually being stored in the computer's memory .
+ The variable , which serves as an easy way to refer to that information for us ,
+ is also reference to the place in a computer's memory where this data is stored .
+ Putting information or data in a variable like this , is called ,
+ `assigning data to a variable` .
+ Assigning is the act of associating some data on the right hand side
+ with a name of a variable on the left hand side ,
+ and we use the `assignment operator` to do this .
+ When we create a new variable ,
+ we are `declaring a variable` .
+ Declaring a variable means,
+ creating an entirely new one ,
+ giving it a name
+ and assigning some data to it .
+ */
+// Keyword var + Name + Assignment operator = + Data
+/**
+ `NOTE`:
+ • The keyword `var` is only used to create a brand new variable.
+ • Unlike boxes in the real world ,
+ once you assign a value or data to a variable ,
+ it forever can only hold that type of data .
+ 
+ Swift uses a set of reserved keywords to define structure in our programs .
+ The first reserved keyword we learned about is `var` .
+ `var` is used to declare a variable .
+ A variable is a container for data .
+ And the reason we call it a variable , is ,
+ because the container can hold different values .
+ A value is a simple data point
+ like text or a number .
+ Every variable we create must be given a name .
+ We then use this name to refer to data inside the container .
+ */
