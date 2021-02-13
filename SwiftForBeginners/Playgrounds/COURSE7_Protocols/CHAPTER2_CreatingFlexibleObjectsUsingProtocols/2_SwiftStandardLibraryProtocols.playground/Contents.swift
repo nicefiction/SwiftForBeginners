a14e86c807fc9798071ceede993239cc77183853
Olivier Van hamme <nicefiction@gmail.com>
2021-02-13 11:43:02 +0100

    Adds Greg Heo's notes from the 55 Swift Standard Library Protocols video .

--- SwiftForBeginners/Playgrounds/COURSE7_Protocols/CHAPTER2_CreatingFlexibleObjectsUsingProtocols/2_SwiftStandardLibraryProtocols.playground/Contents.swift
+++ SwiftForBeginners/Playgrounds/COURSE7_Protocols/CHAPTER2_CreatingFlexibleObjectsUsingProtocols/2_SwiftStandardLibraryProtocols.playground/Contents.swift
@@ -268,3 +268,227 @@ ozma == dorothy
  And if we scroll down , you’ll see that it has an initialisser that takes a `nil` value .
  This is what allows us to assign `nil` as an initial value to an `Optional` type .
  */
+
+
+/* * * * *
+ * EXTRA :
+ */
+
+/**
+ `GREG HEO : WHAT THE 55 SWIFT STANDARD LIBRARY PROTOCOLS THAUGHT ME `
+ 🔗 https://youtu.be/_kpYI6GjCuw
+ */
+/**
+ `GREG HEO : Three types of protocols` PART 1 of 3
+ */
+/**
+ With single inheritance ,
+ class hierarchies things are linear :
+ */
+
+class Animal {}
+    class Feline: Animal {}
+        class Jagwyre: Feline {}
+        class Liger: Feline {}
+    class Canine: Animal {}
+        class Doge: Canine {}
+        class Pug: Canine {}
+
+/**
+ You have got parents , child , grandchild , all the way down the family tree .
+ And when you go back up the tree ,
+ then everything has a single parent .
+ That keeps the hierarchy clean.
+ However, with single inheritance ,
+ ( 1 ) you do lose the benefits that multiple inheritance can offer when used properly .
+ ( 2 ) In Swift , there is no inheritance for enumerations and struct types
+ — it is class types only .
+ That means you sometimes need to twist yourself into a pretzel
+ to make sense of your types .
+ You can end up with really generic super classes .
+ And then many , many levels down ,
+ you have a class that you can actually instantiate , and use .
+ 
+ So with protocols ,
+ you can make the type system
+ a little bit more compositional .
+ You can clean up the long chain of inheritance .
+ Of course you are going to be trading a long tall chain of inheritance ,
+ for a wide chain of protocol conformance .
+ But I think the tradeoffs are worth it ,
+ and I hope you'll be convinced by the end of this talk .
+ What kind of things make sense to put into protocols ?
+ I'm not going to talk about cool protocols that I have made .
+ But instead I want to take a tour of the protocols that Apple has shipped
+ in the Swift Standard Library . We'll take a little tour ,
+ maybe you'll learn about some protocols that you haven't heard of before .
+ We'll see what big ideas that we can take ,
+ and then maybe get some inspiration
+ on what kinds of things we can use with protocols in our own code .
+ Basically the idea of this talk is
+ how does the way the Swift team uses protocols ,
+ give us hints on how we can do it ?
+ The Swift Standard Library includes 54 actual public protocols .
+ My initial jokey pitch for this talk , was ,
+ I would go through them one by one , taking 16 seconds each to fill the time ,
+ but instead ,
+ I have broken them up into three categories :
+
+ ( 1 ) Can Do
+ ( 2 ) Is A
+ ( 3 ) Can Be
+ 
+ We have got the `Can do` protocols ,
+ the `Is a` protocols ,
+ and then the `Can be` protocols .
+ So we'll take them one at a time , see some examples ,
+ and then see what lessons that we can take .
+ */
+/**
+ `1. Can Do protocols`
+ First up we got the `Can do` protocols .
+ These describe things that the type can do or have done to it .
+ They also end in the `-able` syntax
+ which makes them easy to spot as you are browsing through the headers .
+ Here is an example :
+ 
+ `Hashable` ,
+ `Equatable` ,
+ `Comparable` ,
+ 
+ These protocols describe operations that you can perform on the type .
+ There is also a small sub set of protocols in this `can do` group
+ that have to do with alternate views or alternate representations :
+ 
+ `RawRepresentable` ,
+ `CustomPlaygroundQuickLookable`
+ 
+ `NOTE`
+ `CustomPlaygroundQuickLookable` means
+ that your type can be quick looked from a playground .
+ And that means that , again , your type is the same ,
+ you are not really converting it to something else ,
+ but you are providing an alternate view for your value .
+ In this case , it is something that you can display in a quick look .
+ 
+ So we have got _Operations_ ,
+ we have got _Alternate Views_ .
+ */
+/**
+ `2. Is A protocols` [7:30]
+ Next step are the `Is a` protocols ,
+ and these describe what kind of thing the type is .
+ In contrast to the `can do` protocols ,
+ these are more based on identity . That means ,
+ conforming to multiple protocols of this kind
+ feels the closest to something like multiple inheritance in Swift .
+ You can identify these protocols in the Standard Library
+ because they end in the word `-Type` .
+ And this is fully half of the Standard Library ,
+ something like 35 or so of those 54 are of this kind :
+ 
+ `CollectionType` ,
+ `IntegerType` ,
+ `FloatingPointType` ,
+ `BooleanType` ,
+ `ErrorType` ,
+ `SequenceType` ,
+ `GeneratorType`
+ 
+ That means ,
+ once you have these protocols set up ,
+ you can build up what used to be that giant list of super-classes
+ and you instead have a set of protocols , with inheritance if you need it .
+ And then when you construct your type,
+ you can choose which slices of identity here
+ and functionality apply to the type.
+ Since your types can conform to multiple protocols ,
+ this is how you build up the functionality of your type little by little
+ based on protocol conformance .
+ That is the second type of Standard Library protocol .
+ The `Is a` protocols ,
+ having to do with grouping things together and identity .
+ */
+/**
+ `3. Can Be protocols` [11:30]
+ Finally we have the `Can be` types .
+ Rather than just an alternate view of the same thing
+ — as we have already seen —
+ these are more about straight on conversion .
+ Changing from type X
+ over to type Y .
+ And these ones end in the word `-Convertible` .
+ So that means that the type can either be converted from
+ or converted to
+ something else .
+ 
+ `FloatLiteralConvertible`
+ `IntegerLiteralConvertible`
+ `ArrayLiteralConvertible`
+ `CustomStringConvertible`
+ 
+ That is the third type of Standard Library protocol .
+ The `Can be` protocol family ,
+ handling conversions between your types .
+ */
+
+
+/**
+ `GREG HEO : Four broad patterns` PART 2 of 3
+ INTRO — So we have seen three kinds of protocols
+ from the Standard Library
+ having to do with capability , identity , and conversion .
+ What are the broad patterns that we can think about
+ that we have seen from our own code ? We have got four of them :
+ */
+/**
+ `2.1 Operations` [15:30]
+ If there are a common set of operations
+ that you have to perform on your types ,
+ consider breaking these out into a protocol .
+ Related to that was Alternate views ...
+ 
+ `2.2 Alternate views`
+ ... if your type has an alternate view ,
+ or an alternate representation
+ that is not quite a full on conversion ,
+ think about whether it belongs as a common protocol .
+ 
+ `2.3 Identity`
+ For identity ,
+ this is your chance to have something like
+ multiple inheritance , or mix-ins in your types .
+ Thinking about identity and what the types are
+ and grouping similar types together with protocols .
+ 
+ `2.4 Conversions`
+ Finally we have conversions ,
+ whether you convert from a type , or to a type ,
+ if that particular conversion is happening a lot in your code
+ consider breaking that very common conversion out as a protocol
+ to help you keep track of things ,
+ and to keep the interface consistent .
+ */
+
+
+/**
+ `GREG HEO : Closing notes` PART 3 of 3
+ */
+/**
+ Seeing how much Apple has done to move common functionality ,
+ things like map , filter , the enumeration methods , into protocols
+ using just plain old protocols
+ and also protocol extensions ,
+ is a really great example and an inspiration on how powerful the future can be .
+ Apple is following this example .
+ If you look at the definition of Array , for example , it conforms to eight protocols ,
+ String conforms to 12 protocols , and so on .
+ So the idea here is
+ that you are creating these feature bundles in protocols ,
+ and then you can just use them from all over your code base .
+ I think thinking about your types in this way
+ can help you keep things straight and categorised in your head.
+ So , I definitely encourage you to try this out in your own code .
+ Look at your types through the lens of what they have in common using protocols .
+ And that is all I have got . Protocols forever , thank you !
+ */
---
+++ SwiftForBeginners/Playgrounds/COURSE7_Protocols/CHAPTER2_CreatingFlexibleObjectsUsingProtocols/4_Recap.playground/Contents.swift
@@ -0,0 +1,43 @@
+import Foundation
+
+
+/**
+ `5 Recap`
+ INTRO —Yay ! We are done !
+ Let's take a high level look at what we learned in this course .
+ */
+/**
+ A protocol defines a blueprint of
+ methods ,
+ properties ,
+ and other requirements that suit a particular task , or a piece of functionality .
+ Protocols are useful for one main reason ,
+ they allow us to encapsulate common behaviour
+ without having to resort to an inheritance structure .
+ A good way to distinguish when you need to use a protocol , is ,
+ to identify whether the relationship is an `is a` or a `has a` relationship .
+ A car `is a` type of vehicle ,
+ so inheritance is most appropriate here , and car inherits from vehicle .
+ However , a bird has the ability to fly , much like an airplane does . So in this case ,
+ we can factor out this common behaviour and represent it through a protocol .
+ 
+ ⚠️
+ In Swift, protocols are _fully fledged types_ , meaning
+ that we can specify a protocol wherever we typically specify a type .
+ That is , the type of
+ a stored property , constant , variable ,
+ function parameter ,
+ and even return type .
+ 
+ Protocols are even more powerful in Swift .
+ They can inherit from multiple other protocols .
+ We can use this behaviour
+ to build successively more complex protocols
+ to specify certain attributes or behaviour .
+ In doing this we have learned a lot so far ,
+ using protocols or interfaces are
+ one of the more intermediate features of software development .
+ So with this knowledge ,
+ you can start building more complex object graphs
+ and eventually apps with much cooler features .
+ */
